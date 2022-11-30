e8605c1e252c9c26d2c8e881c0a31ce2a38b33b2
Long <lbdyx11@163.com>
2022-11-30 16:10:45 +0800

    push commit

--- git-demo/src/com/itguigu/git/GitTest.java
+++ git-demo/src/com/itguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.itguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hell git!");
+        System.out.println("push git!");
     }
 }
