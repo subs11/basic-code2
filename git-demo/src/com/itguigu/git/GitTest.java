a8539a3a05be8a7f6fd94f6de7b5a9a253009ea7
Long <lbdyx11@163.com>
2022-11-30 16:13:58 +0800

    push2 commit

--- git-demo/src/com/itguigu/git/GitTest.java
+++ git-demo/src/com/itguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hell git!");
         System.out.println("push git!");
+        System.out.println("push2 git!");
     }
 }
